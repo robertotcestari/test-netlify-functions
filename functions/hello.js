6f366a805cc990a20f2a91afd4cafd951b08ed4a
Roberto Cestari <robertotcestari@gmail.com>
2020-04-28 08:37:30 -0300

    mailgun function

--- functions/hello.js
+++ functions/hello.js
@@ -1,24 +1,22 @@
 // require("dotenv").config();
 
 exports.handler = function (event, context, callback) {
+  const api_key = process.env.MAILGUN_API; // deve ser setado no Netlify
+  const domain = process.env.MAILGUN_DOMAIN; // deve ser setado no Netlify
+  const mailgun = require("mailgun-js")({ apiKey: api_key, domain: domain });
 
-    const api_key = process.env.MAILGUN_API;
-    const domain = process.env.MAILGUN_DOMAIN;
-    const mailgun = require("mailgun-js")({ apiKey: api_key, domain: domain });
+  const data = {
+    from: "Beto Teste <me@samples.mailgun.org>",
+    to: "robertotcestari@gmail.com",
+    subject: "Olá das functions!",
+    text: "Testing some Mailgun awesomeness!",
+  };
 
-    const data = {
-      from: "Beto Teste <me@samples.mailgun.org>",
-      to: "robertotcestari@gmail.com",
-      subject: "Olá das functions!",
-      text: "Testing some Mailgun awesomeness!",
-    };
-
-    mailgun.messages().send(data, function (error, body) {
-        callback(null, {
-            statusCode: 200,
-            body: 'Tudo Certo!'
-        } )
-      console.log(body);
+  mailgun.messages().send(data, function (error, body) {
+    callback(null, {
+      statusCode: 200,
+      body: "Tudo Certo!",
     });
-
+    console.log(body);
+  });
 };
