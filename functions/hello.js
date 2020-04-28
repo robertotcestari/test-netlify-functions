07b45f503244e159d92727be4e61a1a3b356b562
Roberto Cestari <robertotcestari@gmail.com>
2020-04-28 08:11:06 -0300

    mailgun function

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,24 @@
+require("dotenv").config();
+
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Olá Capiauzada do bem!",
-  });
+
+    const api_key = process.env.MAILGUN_API;
+    const domain = process.env.MAILGUN_DOMAIN;
+    const mailgun = require("mailgun-js")({ apiKey: api_key, domain: domain });
+
+    const data = {
+      from: "Beto Teste <me@samples.mailgun.org>",
+      to: "robertotcestari@gmail.com",
+      subject: "Olá das functions!",
+      text: "Testing some Mailgun awesomeness!",
+    };
+
+    mailgun.messages().send(data, function (error, body) {
+        callback(null, {
+            statusCode: 200,
+            body: 'Tudo Certo!'
+        } )
+      console.log(body);
+    });
+
 };
